265c5c2ab07cef5b41051deadbc934db598ed0fa
Mikaela Cherry <mcherry3@unl.edu>
2018-10-19 11:36:18 -0500

    updated

--- fish_analysis.R
+++ fish_analysis.R
@@ -2,7 +2,7 @@
 .libPaths("P:/RLibrary")
 library(tidyverse)
 
-data <- read_csv("P:/SNRRclass/2018_10_19/gittuorial/data/Gaeta_etal_CLC_data.csv") 
+data <- read_csv("https://raw.githubusercontent.com/mikaelacherry/gittuorial/master/data/Gaeta_etal_CLC_data.csv") 
 
 data %>% 
   mutate(length_cat = case_when(length >= 300 ~ "big", TRUE ~ as.character("small"))) -> fishdata
