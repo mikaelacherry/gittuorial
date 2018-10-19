8f53ae4e9b5281c0f63bedca4c5e282b5bafc623
Mikaela Cherry <mcherry3@unl.edu>
2018-10-19 11:52:21 -0500

    updated

--- fish_analysis.R
+++ fish_analysis.R
@@ -1,8 +1,15 @@
 
 .libPaths("P:/RLibrary")
 library(tidyverse)
+source("R/themes.R")
 
-data <- read_csv("https://raw.githubusercontent.com/mikaelacherry/gittuorial/master/data/Gaeta_etal_CLC_data.csv") 
+data <- read_csv("P:/SNRRclass/2018_10_19/gittuorial/data/fish_data.csv") 
 
 data %>% 
-  mutate(length_cat = case_when(length >= 300 ~ "big", TRUE ~ as.character("small"))) -> fishdata
+  mutate(length_cat = case_when(length >= 200 ~ "big", TRUE ~ as.character("small"))) -> fishdata
+
+ggplot()+
+  geom_histogram(data = fishdata, aes(x = scalelength))+
+  facet_wrap(~length_cat) + theme_Cherry()
+
+
