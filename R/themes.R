377daf3058235f515cdc3a3f89eb9cc21786f02d
Mikaela Cherry <mcherry3@unl.edu>
2018-10-19 10:52:29 -0500

    updated themes

---
+++ R/themes.R
@@ -0,0 +1,54 @@
+
+
+.libPaths("P:/RLibrary")
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+
+
+
+theme_Cherry <- function(base_size=12, base_family="Helvetica") {
+  library(grid)
+  (theme_bw(base_size = base_size, base_family = base_family)+
+      theme(text=element_text(color="black"),
+            axis.title=element_text(face="bold", size = rel(1.3)),
+            axis.text=element_text(size = rel(1), color = "black"),
+            legend.title=element_text(face="bold"),
+            legend.text=element_text(face="bold"),
+            legend.background=element_rect(fill="transparent"),
+            legend.key.size = unit(0.8, 'lines'),
+            panel.border=element_rect(color="black",size=1),
+            panel.grid=element_blank()
+      ))
+}
+
+
+Paper <- ggplot() + 
+  geom_point(data = mtcars, aes(x = disp, y = mpg)) +
+  theme_Cherry()
+
+
+
+
+
+
+theme_Pres <- function(base_size=20, base_family="Helvetica") {
+  library(grid)
+  (theme_bw(base_size = base_size, base_family = base_family)+
+      theme(text=element_text(color="black"),
+            axis.title=element_text(face="bold", size = rel(1.3)),
+            axis.text=element_text(size = rel(1), color = "black"),
+            legend.title=element_text(face="bold"),
+            legend.text=element_text(face="bold"),
+            legend.background=element_rect(fill="transparent"),
+            legend.key.size = unit(0.8, 'lines'),
+            panel.border=element_rect(color="black",size=1),
+            panel.grid=element_blank()
+      ))
+}
+
+
+Paper <- ggplot() + 
+  geom_point(data = mtcars, aes(x = disp, y = mpg)) +
+  theme_Pres()
+
